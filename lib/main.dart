e9527c49d0e03efe8e51c946225939e4558e8a59
yso009 <yso009@naver.com>
2022-11-11 17:15:48 +0900

    비동기 처리로 데이터 불러오기 testcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -26,16 +29,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future fetch() async {
+    var url =
+        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=37.6297559&lng=127.0177412&m=5';
+
+    var response = await http.get(url);
+    print('Response status :  ${response.statusCode}');
+    print('Response body: ${jsonDecode(utf8.decode(response.bodyBytes))}');
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('testTitle'),
+        title: Text('마스크 재고 있는 곳 : 0곳'),
       ),
       body: Center(
-        child: Text('text'),
-      ),
-
+          child: RaisedButton(
+        onPressed: fetch,
+            child:  Text('테스트'),
+      )),
     );
   }
 }
