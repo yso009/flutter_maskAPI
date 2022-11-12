5cc70ce1782c2f9c3d40cb3ce0b530dc2396998e
yso009 <yso009@naver.com>
2022-11-12 14:47:28 +0900

    feat : add loadingWidget, _buildRemainStatWidget 1. isLoading과 setState를 활용하여 로딩 구현 2. 마스크 갯수에 따른 표시 위젯 구현

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final stores = List<Store>();
+  var isLoading = true;
 
   Future fetch() async {
+    setState(() {
+      isLoading = true;
+    });
+
     var url =
         'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=37.6297559&lng=127.0177412&m=5';
 
@@ -42,25 +47,96 @@ class _MyHomePageState extends State<MyHomePage> {
 
     final jsonStores = jsonResult['stores'];
 
-    stores.clear();
-    jsonStores.forEach((e) {
-      stores.add(Store.fromJson(e));
-
-      // Store store = Store.fromJson(e);
-      // stores.add(store);
+    setState(() {
+      stores.clear();
+      jsonStores.forEach((e) {
+        stores.add(Store.fromJson(e));
+      });
+      isLoading = false;
     });
+    print('fetch 완료');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('마스크 재고 있는 곳 : 0곳'),
+          title: Text('마스크 재고 있는 곳 : ${stores.length}곳'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: fetch,
+            )
+          ],
         ),
-        body: ListView(
-            children: stores.map((e) {
-          return Text(e.remainStat ?? '');
-        }).toList() // null 값을 ''로 표시
-            ));
+        body: isLoading
+            ? loadingWidget()
+            : // isLoading 이면 loadingWidget을 리턴
+            ListView(
+                children: stores.map((e) {
+                return ListTile(
+                  title: Text(e.name),
+                  subtitle: Text(e.addr),
+                  trailing: _buildRemainStatWidget(e),
+                );
+              }).toList() // null 값을 ''로 표시
+                ));
+  }
+
+  Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = '판매중지';
+    var color = Colors.black;
+    if (store.remainStat == 'planty') {
+      remainStat = '충분';
+      description = '100개 이상';
+      color = Colors.green;
+    } switch (store.remainStat) {
+      case 'planty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2 ~ 30개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+      default:
+    }
+
+    return Column(
+      children: [
+        Text(remainStat, style: TextStyle(color: color, fontWeight: FontWeight.bold),),
+        Text(description, style: TextStyle(color: color,))],
+    );
+  }
+
+  Widget loadingWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('정보를 가져오는 중'),
+          CircularProgressIndicator(),
+        ],
+      ),
+    );
   }
 }
