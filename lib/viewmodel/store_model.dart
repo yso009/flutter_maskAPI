471f7f038b4fcc97f140a707aacb12932847ae48
unknown <yso009@naver.com>
2022-11-23 17:13:41 +0900

    notifyLiseteners 추가, fetch 비동기 처리

--- lib/viewmodel/store_model.dart
+++ lib/viewmodel/store_model.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_mask/model/store.dart';
 import 'package:flutter_mask/repository/store_repository.dart';
 
 class StoreModel with ChangeNotifier {
+  List<Store> data = [];
   final _storeRepository = StoreRepository();
 
-  Future fetch() {
-    _storeRepository.fetch();
+  Future fetch() async {
+    data = await _storeRepository.fetch();
+    notifyListeners();
   }
 }
