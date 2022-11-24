0a69bbff789b937f16026f01bd2ea0be415f0c39
unknown <yso009@naver.com>
2022-11-24 10:24:13 +0900

    현재 위치 기반으로 검색 기능 추가 1. repository - location_repository 추가 2. Storemodel - fetch 함수 수정 3. store_repository - StoreRepository 클래스 수정

--- lib/repository/store_repository.dart
+++ lib/repository/store_repository.dart
@@ -4,11 +4,11 @@ import 'dart:convert';
 
 class StoreRepository {
 
-  Future<List<Store>> fetch() async { //List형태인 Store를 return 하겠다. 선언
+  Future<List<Store>> fetch(double lat, double lng) async { //List형태인 Store를 return 하겠다. 선언
     final stores = List<Store>();
 
     var url =
-        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=37.6297559&lng=127.0177412&m=5';
+        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=$lat&lng=$lng&m=5';
 
     var response = await http.get(url);
 
--- lib/viewmodel/store_model.dart
+++ lib/viewmodel/store_model.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_mask/model/store.dart';
+import 'package:flutter_mask/repository/location_repository.dart';
 import 'package:flutter_mask/repository/store_repository.dart';
+import 'package:geolocator/geolocator.dart';
 
 class StoreModel with ChangeNotifier {
   var isLoading = false;
   List<Store> stores = [];
 
   final _storeRepository = StoreRepository();
+  final _locationREpository = LocationRepository();
 
   StoreModel() { // 처음 한번 데이터를 받아줌
     fetch();
@@ -16,7 +19,11 @@ class StoreModel with ChangeNotifier {
     isLoading = true;
     notifyListeners(); // 받은 값을 통지 함
 
-    stores = await _storeRepository.fetch();
+    Position position = await _locationREpository.getCurrentLocation();
+
+    stores = await _storeRepository.fetch(
+        position.latitude,
+        position.longitude);
     isLoading = false;
     notifyListeners(); // 받은 값을 통지 함
   }
